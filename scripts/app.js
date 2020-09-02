8a8e381259ae43171f4e26ed2a430e3475e775de
Liam Callaghan <ljcallaghan64@gmail.com>
2020-09-02 17:31:45 +0100

    bonus timers started

--- scripts/app.js
+++ scripts/app.js
@@ -24,6 +24,61 @@ function init() {
   let stephPosition = 191
   let crakePosition = 591
 
+
+  // let gameTimer = null
+  // let timeCounted = 0
+
+  // function timer(position) {
+  //   if (timeCounted === 10) {
+  //     cells[position].classList.add('survivor')
+  //   }
+  //   if (timeCounted === 15) {
+  //     cells[position].classList.remove('survivor')
+  //   }
+  //   if (timeCounted > 15) {
+  //     endTimer()
+  //     gameTimer = setInterval(() => {
+  //       addBonus()
+  //     }, 1000)
+  //   } else {
+  //     timeCounted++
+  //     console.log(timeCounted)
+  //   }
+  // }
+
+  // function endTimer() {
+  //   clearInterval(gameTimer)
+  // }
+
+
+  // function addBonus() {
+  //   const random = Math.floor(Math.random() * 10) + 1
+  //   console.log(random)
+  //   gameTimer = setInterval(() => {
+  //     if (random === 1) {
+  //       timer(424)
+  //     } else if (random === 2) {
+  //       timer(576)
+  //     } else if (random === 3) {
+  //       timer(100)
+  //     } else if (random === 4) {
+  //       timer(299)
+  //     } else if (random === 5) {
+  //       timer(500)
+  //     } else if (random === 6) {
+  //       timer(659)
+  //     } else if (random === 7) {
+  //       timer(246)
+  //     } else if (random === 8) {
+  //       timer(273)
+  //     } else if (random === 9) {
+  //       timer(364)
+  //     } else {
+  //       timer(395)
+  //     }
+  //   }, 1000)
+  // }
+
   function addPath() {
     const maze = cells.slice(82, 96)
     maze.forEach(cell => createClass(cell))
@@ -275,18 +330,19 @@ function init() {
     cells[position].classList.remove('crake')
   }
 
-  
+
 
   function createGrid() {
     for (let i = 0; i < cellCount; i++) {
       const cell = document.createElement('div')
-      // cell.textContent = i
+      cell.textContent = i
       grid.appendChild(cell)
       cells.push(cell)
     }
     addPath()
     addTeleports()
   }
+
 
   function handleKeyDown(event) {
     removeHack(hackPosition)
@@ -667,13 +723,14 @@ function init() {
 
   function getThePartyStarted() {
     clearInterval(party)
-    party = setInterval(move, 300)
+    party = setInterval(move, 200)
     addHack(hackPosition)
     addJosh(joshPosition)
     addHank(hankPosition)
     addSteph(stephPosition)
     addCrake(crakePosition)
     document.addEventListener('keydown', handleKeyDown)
+    addBonus()
   }
   function stopTheStartedParty() {
     clearInterval(party)
